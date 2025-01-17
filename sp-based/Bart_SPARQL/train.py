c9ccedbe267e5e3f81129a2fcc82d40d56d82cd2
NormalLLer <wanqingsi1438438@gmail.com>
2025-01-17 14:41:04 +0100

    start Bart_SPARQL

--- sp-based/Bart_SPARQL/train.py
+++ sp-based/Bart_SPARQL/train.py
@@ -13,23 +13,22 @@ from .data import DataLoader
 from transformers import BartConfig, BartForConditionalGeneration, BartTokenizer
 from .sparql_engine import get_sparql_answer
 import torch.optim as optim
-import logging
+
 import time
 from utils.lr_scheduler import get_linear_schedule_with_warmup
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s')
-logFormatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
-rootLogger = logging.getLogger()
 import warnings
 warnings.simplefilter("ignore") # hide warnings that caused by invalid sparql query
 
+from loguru import logger
 
 
 
 def train(args):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    logger.info(f"Using device: {device}")
 
-    logging.info("Create train_loader and val_loader.........")
+    logger.info("Create train_loader and val_loader.........")
     vocab_json = os.path.join(args.input_dir, 'vocab.json')
     train_pt = os.path.join(args.input_dir, 'train.pt')
     val_pt = os.path.join(args.input_dir, 'val.pt')
@@ -38,12 +37,12 @@ def train(args):
 
     vocab = train_loader.vocab
     
-    logging.info("Create model.........")
+    logger.info("Create model.........")
     config_class, model_class, tokenizer_class = (BartConfig, BartForConditionalGeneration, BartTokenizer)
     tokenizer = tokenizer_class.from_pretrained(args.model_name_or_path)
     model = model_class.from_pretrained(args.model_name_or_path)
     model = model.to(device)
-    logging.info(model)
+    logger.info(model)
     t_total = len(train_loader) // args.gradient_accumulation_steps * args.num_train_epochs    # Prepare optimizer and schedule (linear warmup and decay)
     no_decay = ["bias", "LayerNorm.weight"]
     bart_param_optimizer = list(model.named_parameters())
@@ -65,30 +64,31 @@ def train(args):
         scheduler.load_state_dict(torch.load(os.path.join(args.model_name_or_path, "scheduler.pt")))
 
     # Train!
-        logging.info("***** Running training *****")
-        logging.info("  Num examples = %d", len(train_loader.dataset))
-        logging.info("  Num Epochs = %d", args.num_train_epochs)
-        logging.info("  Gradient Accumulation steps = %d", args.gradient_accumulation_steps)
-        logging.info("  Total optimization steps = %d", t_total)
+        logger.info("***** Running training *****")
+        logger.info("  Num examples = %d", len(train_loader.dataset))
+        logger.info("  Num Epochs = %d", args.num_train_epochs)
+        logger.info("  Gradient Accumulation steps = %d", args.gradient_accumulation_steps)
+        logger.info("  Total optimization steps = %d", t_total)
 
     global_step = 0
     steps_trained_in_current_epoch = 0
+    epochs_trained = 0
     # Check if continuing training from a checkpoint
     if os.path.exists(args.model_name_or_path) and "checkpoint" in args.model_name_or_path:
         # set global_step to gobal_step of last saved checkpoint from model path
         global_step = int(args.model_name_or_path.split("-")[-1].split("/")[0])
         epochs_trained = global_step // (len(train_loader) // args.gradient_accumulation_steps)
         steps_trained_in_current_epoch = global_step % (len(train_loader) // args.gradient_accumulation_steps)
-        logging.info("  Continuing training from checkpoint, will skip to saved global_step")
-        logging.info("  Continuing training from epoch %d", epochs_trained)
-        logging.info("  Continuing training from global step %d", global_step)
-        logging.info("  Will skip the first %d steps in the first epoch", steps_trained_in_current_epoch)
-    logging.info('Checking...')
-    logging.info("===================Dev==================")
+        logger.info("  Continuing training from checkpoint, will skip to saved global_step")
+        logger.info("  Continuing training from epoch %d", epochs_trained)
+        logger.info("  Continuing training from global step %d", global_step)
+        logger.info("  Will skip the first %d steps in the first epoch", steps_trained_in_current_epoch)
+    logger.info('Checking...')
+    logger.info("===================Dev==================")
     # evaluate(args, model, val_loader, device)
-    tr_loss, logging_loss = 0.0, 0.0
+    tr_loss, logger_loss = 0.0, 0.0
     model.zero_grad()
-    for _ in range(int(args.num_train_epochs)):
+    for _ in range(epochs_trained, int(args.num_train_epochs)):
         pbar = ProgressBar(n_total=len(train_loader), desc='Training')
         for step, batch in enumerate(train_loader):
             # Skip past any already trained steps if resuming training
@@ -120,10 +120,10 @@ def train(args):
                 scheduler.step()  # Update learning rate schedule
                 model.zero_grad()
                 global_step += 1
-            # if args.logging_steps > 0 and global_step % args.logging_steps == 0:
-            #     logging.info("===================Dev==================")
+            # if args.logger_steps > 0 and global_step % args.logger_steps == 0:
+            #     logger.info("===================Dev==================")
             #     evaluate(args, model, val_loader, device)
-            #     logging.info("===================Test==================")
+            #     logger.info("===================Test==================")
             #     evaluate(args, model, test_loader, device)
             if args.save_steps > 0 and global_step % args.save_steps == 0:
                     # Save model checkpoint
@@ -135,12 +135,12 @@ def train(args):
                     )  # Take care of distributed/parallel training
                     model_to_save.save_pretrained(output_dir)
                     torch.save(args, os.path.join(output_dir, "training_args.bin"))
-                    logging.info("Saving model checkpoint to %s", output_dir)
+                    logger.info("Saving model checkpoint to %s", output_dir)
                     tokenizer.save_vocabulary(output_dir)
                     torch.save(optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt"))
                     torch.save(scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
-                    logging.info("Saving optimizer and scheduler states to %s", output_dir)
-        logging.info("\n")
+                    logger.info("Saving optimizer and scheduler states to %s", output_dir)
+        logger.info("\n")
         if 'cuda' in str(device):
             torch.cuda.empty_cache()
     return global_step, tr_loss / global_step
@@ -161,9 +161,9 @@ def main():
     parser.add_argument('--batch_size', default=8, type=int)
     parser.add_argument('--seed', type=int, default=666, help='random seed')
     parser.add_argument('--learning_rate', default=3e-5, type = float)
-    parser.add_argument('--num_train_epochs', default=25, type = int)
+    parser.add_argument('--num_train_epochs', default=30, type = int)
     parser.add_argument('--save_steps', default=448, type = int)
-    parser.add_argument('--logging_steps', default=448, type = int)
+    parser.add_argument('--logger_steps', default=448, type = int)
     parser.add_argument('--warmup_proportion', default=0.1, type = float,
                         help="Proportion of training to perform linear learning rate warmup for,E.g., 0.1 = 10% of training.")
     parser.add_argument("--adam_epsilon", default=1e-8, type=float,
@@ -181,15 +181,19 @@ def main():
     parser.add_argument('--alpha', default = 1e-4, type = float)
     args = parser.parse_args()
 
-    if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir)
-    time_ = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
-    fileHandler = logging.FileHandler(os.path.join(args.save_dir, '{}.log'.format(time_)))
-    fileHandler.setFormatter(logFormatter)
-    rootLogger.addHandler(fileHandler)
+    # # make logger.info display into both shell and file
+    # if os.path.isdir(args.save_dir):
+    #     shutil.rmtree(args.save_dir)
+    os.makedirs(args.save_dir, exist_ok=True)
+
+    # fileHandler = logger.FileHandler(os.path.join(args.save_dir, 'log.txt'))
+    # fileHandler.setFormatter(logFormatter)
+    # rootLogger.addHandler(fileHandler)
+    logger.add(os.path.join(args.save_dir, 'log.txt'), format="{time} {level:8} {message}")
+
     # args display
     for k, v in vars(args).items():
-        logging.info(k+':'+str(v))
+        logger.info(k+': '+str(v))
 
     seed_everything(666)
 
--- sp-based/SPARQL/train.py
+++ sp-based/SPARQL/train.py
@@ -158,15 +158,15 @@ def train(args):
     reload = False
 
     # load model if resume_training is True, otherwise create a new model
-    if args.resume_training == "True":
+    if args.resume_training.lower() == "true":
         model_path = os.path.join(args.save_dir, args.resume_model)
         if os.path.exists(model_path):
-            logger.info(f"Loading model parameters trained with {args.resume_epoch} epochs, from {model_path}")
+            logger.info(f"Resuming training from {model_path}, starting at epoch {args.resume_epoch}")
             model.load_state_dict(torch.load(model_path, map_location=device))
             logger.info(f"Model loaded on {device}.")
             reload = True
         else:
-            logger.warning(f"No model found at {model_path}, starting from scratch")
+            logger.warning(f"Resume model path {model_path} does not exist. Starting fresh training.")
             logger.info("Create model.........")
 
     logger.info(model)
